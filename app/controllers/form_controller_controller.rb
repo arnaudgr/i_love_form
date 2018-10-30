e70f4c9dc4894a48182c8a7e57bf18bda76aefc2
Arnaud <arnaud.grandval@gmail.com>
2018-10-30 21:15:25 +0100

    V1

--- app/controllers/form_controller_controller.rb
+++ app/controllers/form_controller_controller.rb
@@ -1,16 +1,20 @@
 class FormControllerController < ApplicationController
   def home
 
-  	# @user = User.new
+  	@user = User.new
   
 
   end
 
   def create
-   #  @user.save
+  	# user = User.new
+   #  user.username = params["user"]["username"]
+   #  user.email = params["user"]["email"]
+   #  user.bio = params["user"]["bio"]
+
   	# puts "hello Arnaud"
     # user = @user.save
      @user = User.new(username: params[:username], email: params[:email], bio: params[:bio])
-   #   @user.save
+     @user.save
   end
 end
